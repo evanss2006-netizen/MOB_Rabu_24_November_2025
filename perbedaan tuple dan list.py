f391fef57c07be15dfa7d300263c4b4239924ec7
Evans <evanss2006@gmail.com>
2025-11-26 09:36:09 +0800

    Revise list and tuple examples in Python script
    Updated examples of list and tuple with fruit names.

--- perbedaan tuple dan list.py
+++ perbedaan tuple dan list.py
@@ -1,19 +1,26 @@
 #list(data bisa diedit)
-list_a = [6, 9, 4, 8, 3, 1, 5, 7]
+#list_a = [6, 9, 4, 8, 3, 1, 5, 7]
 #tuple(data bersifat tetap tidak bisa diedit)
-tuple_a = (6, 9, 4, 8, 3, 1, 5, 7)
+#tuple_a = (6, 9, 4, 8, 3, 1, 5, 7)
 
 #index(cara hitung data) ada 2 yaitu positif dan negatif
 #positif dimulai dari kiri ke kanan (dimulai dari 0)
 #negatif dimulai dari kanan ke kiri (dimulai dari -1)
 
+#contoh
+#list
+list_a = ['mangga', 'pisang', 'jeruk', 'jambu', 'apel', 'semangka', 'anggur']
+#tuple
+tuple_a = ('mangga', 'pisang', 'jeruk', 'jambu', 'apel', 'semangka', 'anggur')
+
 print(list_a[4]) #dihitung secara positif
 print(tuple_a[-5]) #dihitung secara negatif
 
 #untuk mencek perintah yang dapat digunakan dalam list dan tuple
-print(20*'_')
+#print(20*'_')
 #print(dir(list_a)) # semuah perintah yang tidak menggunakan underscore(_) adalah fungsi yang dapat digunakan list
 #print(der(tuple_a)) # semuah perintah yang tidak menggunakan underscore(_) adalah fungsi yang dapat digunakan tuple
+
 
 
 
